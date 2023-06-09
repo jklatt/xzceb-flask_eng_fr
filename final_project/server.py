daef589b9c3f070a2c3de565596195b63e324967
JK <juliane.klatt@pm.me>
2023-06-09 23:02:36 +0200

    Final changes

--- final_project/server.py
+++ final_project/server.py
@@ -18,7 +18,7 @@ def frenchToEnglish():
 
 @app.route("/")
 def renderIndexPage():
-    render_template('index.html')
+    render_template('templates/index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
