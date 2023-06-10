fd3ef34edd3a7f39b00bc79dae2a1401305e4158
JK <juliane.klatt@pm.me>
2023-06-10 20:20:40 +0200

    Final changes

--- final_project/server.py
+++ final_project/server.py
@@ -18,7 +18,9 @@ def frenchToEnglish():
 
 @app.route("/")
 def renderIndexPage():
-    render_template('templates/index.html')
+    render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    #app.run(host="0.0.0.0", port=8080)
+    app.run(debug=True)
+    # When no port is specified, starts at default port 5000
